579bf235e6eaeb2a52af7ee503300cbf4eabf047
Oscar Sierra <o_sierra@farmacity.com.ar>
2022-06-07 18:23:50 -0500

    fix code

--- PruebaAutomatizada/Program.cs
+++ PruebaAutomatizada/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PruebaAutomatizada
@@ -19,19 +20,19 @@ namespace PruebaAutomatizada
             //_driver = new FirefoxDriver(Directory.GetCurrentDirectory());
             driver.Navigate().GoToUrl("https://www.segurodeviajemundial.com.co/");
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.XPath("//option[contains(text(),'Europa y África')]")).Click();
-            driver.FindElement(By.XPath("//a[contains(text(),'31')]")).Click();
+            Thread.Sleep(5000);
+
+            driver.FindElement(By.XPath("//option[contains(text(),'Latinoamerica. Caribe y Cánada (No USA)')]")).Click();
             driver.FindElement(By.XPath("//input[@id='fecha_regreso']")).Click();
             driver.FindElement(By.XPath("//option[contains(text(),'Jul')]")).Click();
             driver.FindElement(By.XPath("//a[contains(text(),'20')]")).Click();
+            driver.FindElement(By.XPath("//input[@id='fecha_regreso']")).Click();
+            driver.FindElement(By.XPath("//body/div[@id='ui-datepicker-div']/div[1]/div[1]/select[1]")).Click();
+            driver.FindElement(By.XPath("//option[contains(text(),'Aug')]")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'24')]")).Click();
             driver.FindElement(By.XPath("//tbody/tr[2]/td[2]/div[1]/input[1]")).SendKeys("juliandavidsanchezsanchez@gmail.com");
+            Thread.Sleep(5000);
             driver.FindElement(By.XPath("//tbody/tr[2]/td[1]/button[1]")).Click();
-            
-
-
-
-
-
 
         }
     }
